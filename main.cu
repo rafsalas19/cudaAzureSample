bc3cc2fafb9fe2e3ef8c84d3e8ac2bf44ebf3bb8
Rafael Salas <rafsalas1987@hotmail.com>
2021-03-16 10:48:06 -0400

    Fresh commit. GPU code that mimics a Python code I found and added. Sill not working completely but this is a good start.

--- main.cu
+++ main.cu
@@ -5,17 +5,153 @@
 #include <math.h>
 #include <stdio.h>
 #include <iostream>;
+#include <fstream>
 #include <string>
 
 using namespace std;
 
+#define ITERATION 2
+#define LENX 10
+#define LENY 10 
+
+void checkError(cudaError err) {
+	if (err != cudaSuccess)
+	{
+		fprintf(stderr, "Cuda error (error code %s)!\n", cudaGetErrorString(err));
+		exit(EXIT_FAILURE);
+	}
+
+}
+
+__global__ void
+transferHeat(const float* inGrid, float* outGrid)
+{
+	int i = blockDim.x * blockIdx.x + threadIdx.x;
+	if (i > LENX * LENY) {
+		return;
+	}
+
+
+
+	if (i > LENX && i <(LENX * LENY -1)-LENX)
+	{		
+		outGrid[i] = .25 * (inGrid[i-1] + inGrid[i+1] + inGrid[i + LENY] + inGrid[i - LENY]);
+		//30,30,30, 75
+		if(i==29)printf("%f | %f %f %f %f\n",outGrid[i], inGrid[i - 1], inGrid[i + 1], inGrid[i + LENY], inGrid[i - LENY]);
+	}
+	else
+	{
+		outGrid[i] = inGrid[i];
+	}
+}
+
+void default_initialize(float * gridPointsCPU, int lenX, int lenY) {
+	for (int j = 0;j<lenY;++j) {
+		for (int i = 0; i < lenX; ++i) {
+			int point = i + j * lenX;
+			if (j == 0) {
+				gridPointsCPU[point] = 100;
+			}
+
+			else if (j == lenY-1) {
+				gridPointsCPU[point] = 0;
+			}
+			else if (j ==0 && i%lenX==0) {
+				gridPointsCPU[point] = 0;
+			}
+			else if (j == lenY - 1 && i % lenX == lenX-1) {
+				gridPointsCPU[point] = 0;
+			}
+			else {
+				gridPointsCPU[point] = 30;
+			}
+		}
+	}
+
+	return;
+}
 
 
 int main(){
-	
+	int probSize = LENX * LENY;	
+	float gridPointsCPU[LENX * LENY] = {0};
+	float* gridPointsOutGPU = NULL;
+	float* gridPointsInGPU = NULL;
 	int devicecount = 0;
+
+	//check device count
 	cudaGetDeviceCount(&devicecount);
 	cout << "Device count is " << devicecount << endl;
+	cudaError err = cudaSuccess;
+
+	err = cudaMalloc((void**)&gridPointsInGPU, probSize * sizeof(float));
+	checkError(err);
+	err = cudaMalloc((void**)&gridPointsOutGPU, probSize * sizeof(float));
+	checkError(err);
+
+	//initialize
+	default_initialize(gridPointsCPU,LENX,LENY);
+
+	//copy from cpu
+	err = cudaMemcpy(gridPointsInGPU, gridPointsCPU, probSize, cudaMemcpyHostToDevice);
+	checkError(err);
+
+	fstream fstartBuffer;
+	fstartBuffer.open("htStart", fstream::out | fstream::trunc);
+	for (int i = 0; i < probSize; ++i) {
+		//fstartBuffer << gridPointsCPU[i] << endl;
+		int x = i % LENX;
+		int y = i / LENY;
+		fstartBuffer << "index: " << i << " point:" << x << "," << y << " " << gridPointsCPU[i] << endl;
+	}
+	fstartBuffer.close();
+
+
+	int gridSize = 0;
+	int blockSize = 128;
+	gridSize = probSize/blockSize +1;
+	for (int i = 0; i < ITERATION ; ++i) {
+
+
+
+		transferHeat << < gridSize, blockSize >> > (gridPointsInGPU, gridPointsOutGPU);
+		err = cudaDeviceSynchronize();
+
+
+		//err = cudaMemcpy(gridPointsCPU, gridPointsOutGPU, probSize, cudaMemcpyDeviceToHost);
+		//checkError(err);
+		//fstream fouti;
+		//fouti.open("htlocalout_" + to_string(i), fstream::out | fstream::trunc);
+		//for (int i = 0; i < probSize; ++i) {
+		//	int x = i % LENX;
+		//	int y = i / LENY;
+		//	fouti << "index: " << i << " point:" << x << "," << y << " " << gridPointsCPU[i] << endl;
+		//}
+		//fouti.close();
+
+
+		if (i < ITERATION - 1) {
+			float* swap = gridPointsInGPU;
+			gridPointsInGPU = gridPointsOutGPU;
+			gridPointsOutGPU = swap;
+		}
+	}
+	
+	err = cudaGetLastError();
+	checkError(err);
+	err = cudaDeviceSynchronize();
+	checkError(err);
+	err = cudaMemcpy(gridPointsCPU, gridPointsInGPU, probSize, cudaMemcpyDeviceToHost);
+	checkError(err);
+
+	fstream fout;
+	fout.open("htOut", fstream::out | fstream::trunc);
+	for (int i = 0;i<probSize;++i) {
+		int x = i % LENX;
+		int y = i / LENY;
+		fout <<"index: "<< i <<" point:" <<x <<","<<y<<" "<<gridPointsCPU[i] << endl;
+	}
+	fout.close();
 	char endChar = 'c';
 	cout << "enter a character to exit"<<endl;
 	cin >> endChar;
